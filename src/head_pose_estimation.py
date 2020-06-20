af9aa231cf2dc81b5f1a8adc7051d9c30b5f9b6c
Ankit <ankitvashisht12@gmail.com>
2020-06-20 13:07:03 +0530

    fix output

--- src/head_pose_estimation.py
+++ src/head_pose_estimation.py
@@ -88,8 +88,8 @@ class Model_Head_Pose_Estimation:
         Before feeding the output of this model to the next model,
         you might have to preprocess the output. This function is where you can do that.
         '''
-        p = outputs["angle_p_fc"]
-        r = outputs["angle_r_fc"]
-        y = outputs["angle_y_fc"]
+        p = outputs["angle_p_fc"][0][0]
+        r = outputs["angle_r_fc"][0][0]
+        y = outputs["angle_y_fc"][0][0]
       
         return (p, r, y)
