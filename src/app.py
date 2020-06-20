97921a9c12d7a72b182e13fad7cccfb9622fca54
Ankit <ankitvashisht12@gmail.com>
2020-06-20 12:39:20 +0530

    Cropped left and right eye for gaze detection

--- src/app.py
+++ src/app.py
@@ -13,12 +13,21 @@ def get_args():
     parser.add_argument("-m1", "--face_detection", required=True, type=str, help="Path to face detection")
     parser.add_argument("-m2", "--landmark_detection", required=True, type=str, help="Path to landmark detection model")
     parser.add_argument("-m3", "--head_pose_detection", required=True, type=str, help="Path to head pose detection model")
-    # parser.add_argument("-m4", "--gaze_detection", required=True, type=str, help="Path to gaze detection model")
+    parser.add_argument("-m4", "--gaze_detection", required=True, type=str, help="Path to gaze detection model")
     parser.add_argument("-d", "--device", required=False, type=str, help="Specify device", default="CPU")
     parser.add_argument("-e", "--extention", required=False, type=str, help="Specify CPU extention", default=None)
     parser.add_argument("-i", "--input_file", required=True, type=str, help="Specify input file type", default="cam")
     parser.add_argument("-p", "--input_path", required=False, type=str, help="Specify input file path", default=None)
     return parser.parse_args()
+
+def crop_eyes(frame, coords, start_point, end_point):
+   
+    l  = 40 + abs(coords[1][0] - coords[0][0])
+    y = coords[0][1] - l//2
+    left_eye = frame[y : y + l , start_point[0] : start_point[0]+ l, :]
+    right_eye = frame[y : y + l, end_point[0]-l : end_point[0], :]
+   
+    return left_eye, right_eye
 
 
 
@@ -31,13 +40,13 @@ def main():
     fd = Model_Face_Detection(args.face_detection, args.device, args.extention)
     ld = Model_Facial_Landmark_Detection(args.landmark_detection, args.device, args.extention)
     hp = Model_Head_Pose_Estimation(args.head_pose_detection, args.device, args.extention)
-    #gd = Model_Gaze_Estimation(args.gaze_detection, args.device, args.extention)
+    gd = Model_Gaze_Estimation(args.gaze_detection, args.device, args.extention)
 
     # load models
     fd.load_model()
     ld.load_model()
     hp.load_model()
-    #gd.load_model()
+    gd.load_model()
 
     # Initialize input feed and load data
     input_feed = InputFeeder(args.input_file, args.input_path)
@@ -46,11 +55,12 @@ def main():
 
     for frame in input_feed.next_batch():
         
-        outs_fd = fd.predict(frame) 
+        outs_fd = fd.predict(frame.copy()) 
         cropped_face = []
       
         if len(outs_fd) != 0:
-
+            
+            # extract coordinates for bounding box
             xmin = outs_fd[0][0]
             ymin = outs_fd[0][1]
             xmax = outs_fd[0][2]
@@ -58,9 +68,11 @@ def main():
             start_point = (xmin, ymin)
             end_point = (xmax, ymax)
             color = (255, 0, 0)
-
+            
+            # draw bounding box around the face
             show_frame = cv2.rectangle(img=frame, pt1=start_point, pt2=end_point , color=color , thickness=2)
 
+            # crop face
             delta_y = ymax - ymin
             delta_x = xmax - xmin
             cropped_face = frame[ymin:ymin+delta_y, xmin:xmin+delta_x]
@@ -70,25 +82,37 @@ def main():
             # cv2.waitKey(5)
 
            
-
+            # predict facial landmark on cropped image 
             outs_ld = ld.predict(cropped_face.copy())
             if len(outs_ld) != 0:
-            
-
+                
+                # extract coordinates for left and right eye 
                 p1 = tuple(sum(x) for x in zip(outs_ld[0][0], start_point)) 
                 p2 = tuple(sum(x) for x in zip(outs_ld[0][1], start_point))
                 p3 = tuple(sum(x) for x in zip(outs_ld[0][2], start_point))
                 p4 = tuple(sum(x) for x in zip(outs_ld[0][3], start_point))
+
+                left_eye, right_eye = crop_eyes(frame.copy(), [p1, p2, p3, p4], start_point, end_point)
+
+                # Uncomment to show the eye
+                # cv2.imshow("right eye", right_eye)
+                # key = cv2.waitKey(1)
+                # if key == ord('q'):
+                #     break
                 
+                # draw circle on the detected coordinates of left and right eye
                 show_frame = cv2.circle(show_frame, p1, 2, (0, 0, 255), thickness=-5)
                 show_frame = cv2.circle(show_frame, p2, 2, (0, 0, 255), thickness=-5)
                 show_frame = cv2.circle(show_frame, p3, 2, (0, 0, 255), thickness=-5)
                 show_frame = cv2.circle(show_frame, p4, 2, (0, 0, 255), thickness=-5)
             
-            
-            p, r, y = hp.predict(cropped_face.copy())  # pitch, roll, yaw
+            # pitch, roll and yaw estimation on cropped face
+            p, r, y = hp.predict(cropped_face.copy())  
                 
-
+            # gaze estimation
+            outs_gd = gd.predict(left_eye, right_eye, np.array([[p, r, y]]))
+            
+            
         else:
             show_frame = frame
         
